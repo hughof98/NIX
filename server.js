8447f70339a63179d1c475183d832932c43b602c
hughof98 <hughof98@gmail.com>
2024-10-25 12:15:21 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-// Serve the HTML file and static files (like CSS, images)
-app.use(express.static(__dirname));
+// Use the environment variable PORT or default to 10000 for local testing
+const PORT = process.env.PORT || 10000;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -15,18 +12,14 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-
-    // Receive and broadcast chat message
-    socket.on('chat message', (data) => {
-        io.emit('chat message', data);
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
     });
-
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+http.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
