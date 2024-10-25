222d3e9e8d1e643a560ee160c3483a9180e01bf2
hughof98 <hughof98@gmail.com>
2024-10-25 13:29:47 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -6,20 +6,29 @@ const io = require('socket.io')(http);
 // Use the environment variable PORT or default to 10000 for local testing
 const PORT = process.env.PORT || 10000;
 
+// Serve static files (like your index.html)
+app.use(express.static(__dirname));
+
+// Serve the index.html file
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+// Socket.io connection
 io.on('connection', (socket) => {
     console.log('a user connected');
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+
+    // Listen for chat messages
+    socket.on('chat message', (data) => {
+        io.emit('chat message', data); // Broadcast the message to all connected clients
     });
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 });
 
+// Start the server
 http.listen(PORT, () => {
-    console.log(`Server running on https://nix-6o2o.onrender.com`);
+    console.log(`Server running on port ${PORT}`);
 });
